4b893939cbd16dce8911a570cdba939427d95cae
rattanapoom <614259058@webmail.npru.ac.th>
2021-03-19 11:59:39 +0700

    my commit

---
+++ application/models/Menu_dental.php
@@ -0,0 +1,98 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Menu_dental extends CI_Model {
+		function __construct(){
+            parent::__construct();
+        }
+        function insert_user($data){
+			$this->db->insert('user',$data);
+        }
+        function insert_dentalitems($data){
+			$this->db->insert('dentalitems',$data);
+        }
+        
+        function get_book($idUser){
+            $this->db->select('*');
+            $this->db->from('dentalitems');
+           
+            $this->db->where('idUser', $idUser);
+            $this->db->where('status', "รอเจ้าหน้าที่ตรวจสอบ");
+            
+            $query = $this->db->get();
+            return $query->result();
+            }
+
+            function get_doc($name){
+                $this->db->select('*');
+                $this->db->from('dentalitems');
+                $this->db->where('idDoctor', $name);
+               
+                $this->db->join('user', 'user.idUser = dentalitems.idUser', 'left');
+               
+               
+                $query = $this->db->get();
+                return $query->result();
+                }
+
+            function get_his($idUser){
+                $this->db->select('*');
+                $this->db->from('dentalitems');
+               
+                $this->db->where('idUser', $idUser);
+                $this->db->where('status', "สำเร็จ");
+                
+                $query = $this->db->get();
+                return $query->result();
+                }
+
+                function get_admin(){
+                    
+                $this->db->select('*');
+                $this->db->from('dentalitems');
+                $this->db->join('user', 'user.idUser = dentalitems.idUser', 'left');
+                $query = $this->db->get();
+                return $query->result();
+                }
+
+                function get_profile($idUser){
+                    $this->db->select('*');
+                    $this->db->from('user');
+                   
+                    $this->db->where('idUser', $idUser);
+                    
+                    $query = $this->db->get();
+                    return $query->result();
+                    }
+
+                function get_update($idDental)
+                {
+                $query=$this->db->query("select * from dentalitems where idDental='".$idDental."'");
+                return $query->result();
+                }
+
+                function update_records($idDoctor,$status,$idDental)
+	{
+	$query=$this->db->query("update dentalitems SET idDoctor='$idDoctor',status='$status' where idDental='$idDental'");
+	}
+
+                function Update_profile($data)
+                {
+                 $idUser =  $this->session->userdata('idUser');
+                 $this->db->where('idUser', $idUser);
+                  $this->db->update('user', $data);
+                 }
+
+
+    function validate($email,$password){
+        $this->db->where('email',$email);
+        $this->db->where('password',$password);
+        $result = $this->db->get('user',1);
+        return $result;
+      }
+
+
+     
+
+
+
+       
+}
